5eb1a4240ec3041059aad620865b8ca2503d17da
ipaljak <ipaljak@gmail.com>
2016-12-08 14:21:53 +0100

    Day7 solved (cpp)

---
+++ day8/part2.cpp
@@ -0,0 +1,66 @@
+#include <cstdio>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+const int R = 6;
+const int C = 50;
+
+char grid[R][C+1];
+
+inline void rect(int r, int c) {
+  for (int i = 0; i < r; ++i)
+    for (int j = 0; j < c; ++j)
+      grid[i][j] = '#';
+}
+
+inline void rot_row(int r, int n) {
+  vector <char> tmp;
+  for (int i = 0; i < C; ++i)
+    tmp.push_back(grid[r][(C - n + i) % C]);
+  for (int i = 0; i < C; ++i)
+    grid[r][i] = tmp[i];
+}
+
+inline void rot_col(int c, int n) {
+  vector <char> tmp;
+  for (int i = 0; i < R; ++i)
+    tmp.push_back(grid[(R - n + i) % R][c]);
+  for (int i = 0; i < C; ++i)
+    grid[i][c] = tmp[i];
+}
+
+inline void trace_grid() {
+  for (int i = 0; i < R; ++i) {
+    for (int j = 0; j < C; ++j)
+      printf("%c", grid[i][j]);
+    printf("\n");
+  }
+}
+
+int main(void) {
+
+  memset(grid, '.', sizeof grid);
+
+  char oper[10];
+  while (scanf("%s", oper) != EOF) {
+    if (strcmp(oper, "rect") == 0) {
+      int c, r; scanf("%dx%d", &c, &r);
+      rect(r, c);
+    } else {
+      char dummy[20], dim;
+      int x, n;
+      scanf("%s %c=%d by %d", dummy, &dim, &x, &n);
+      if (dim == 'x')
+        rot_col(x, n);
+      else
+        rot_row(x, n);
+    }
+  }
+
+  trace_grid();
+
+  return 0;
+
+}
